0d5b192e91394c42a1f4dd506a6122aefa03d818
tania koziuk <taniakoziuk@gmail.com>
2018-09-05 22:42:32 +0300

    empty pass test added

---
+++ ProjectDemo/tests/emptyPass_test.js
@@ -0,0 +1,16 @@
+let LoginPage = require("../page_objects/login.page");
+
+describe('login with empty pass', function() {
+    it('should not login', async function() {
+        let loginPage = new LoginPage();
+        
+        await loginPage.open();
+        await loginPage.getLoginButton().click();
+        await loginPage.getEmailinput().sendKeys("taniakoziuk@gmail.com");
+        await loginPage.getSignInButton().click();
+        
+        let signInBtn = element(by.css(".iframe-wrap .login-button"));
+        expect(await signInBtn.isEnabled()).toBe(false);
+
+    });
+});
