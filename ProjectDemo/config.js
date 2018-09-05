7d7243779cb33dac881a7b87d7a3aa3231aac7ba
tania koziuk <taniakoziuk@gmail.com>
2018-09-05 21:53:26 +0300

    allure reports added

--- ProjectDemo/config.js
+++ ProjectDemo/config.js
@@ -14,9 +14,24 @@ exports.config = {
     },
 
     onPrepare: function() {
-        console.log("The test is started");
+      let AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
     },
   
+    onPrepare: function () {
+      let AllureReporter = require('jasmine-allure-reporter');
+      jasmine.getEnv().addReporter(new AllureReporter());
+      jasmine.getEnv().afterEach(function(done){
+        browser.takeScreenshot().then(function (png) {
+          allure.createAttachment('Screenshot', function () {
+            return new Buffer(png, 'base64')
+          }, 'image/png')();
+          done();
+        })
+      });
+    },
     // Spec patterns are relative to the configuration file location passed
     // to protractor (in this example conf.js).
     // They may include glob patterns.
