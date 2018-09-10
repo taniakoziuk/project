460383f53dddfdceaf132380d46f7961af447230
Tetiana Koziuk <tetiana.koziuk@eleks.com>
2018-09-10 21:34:34 +0300

    allure steps added

--- ProjectDemo/tests/emptyPass_test.js
+++ ProjectDemo/tests/emptyPass_test.js
@@ -4,10 +4,17 @@ describe('login with empty pass', function() {
     it('should not login', async function() {
         let loginPage = new LoginPage();
         
-        await loginPage.open();
-        await loginPage.getLoginButton().click();
-        await loginPage.getEmailinput().sendKeys("taniakoziuk@gmail.com");
-        await loginPage.getSignInButton().click();
+        await allure.createStep("Open login page", async() => 
+        await loginPage.open())();
+
+        await allure.createStep("Click login button", async() =>
+        await loginPage.getLoginButton().click())();
+
+        await allure.createStep("enter email", async() =>
+        await loginPage.getEmailinput().sendKeys("taniakoziuk@gmail.com"))();
+
+        await allure.createStep("click Sign In button", async() =>
+        await loginPage.getSignInButton().click())();
         
         let signInBtn = element(by.css(".iframe-wrap .login-button"));
         expect(await signInBtn.isEnabled()).toBe(false);
